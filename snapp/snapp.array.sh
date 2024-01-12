8599b8a8bfc5c4135d8d2a9ee0371487a3056bd6
Devon DeRaad <devonderaad@gmail.com>
2024-01-12 17:13:43 -0600

    Add SNAPP

---
+++ snapp/snapp.array.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+#SBATCH --job-name=snapp               # Job Name
+#SBATCH --nodes=1             # 40 nodes
+#SBATCH --ntasks-per-node=15               # 40 CPU allocation per Task
+#SBATCH --partition=bi            # Name of the Slurm partition used
+#SBATCH --chdir=/home/d669d153/work/phil.dicaeum/snapp    # Set working d$
+#SBATCH --mem-per-cpu=800            # memory requested
+#SBATCH --array=1-5
+#SBATCH --time=10000
+
+#run beast 2.7.1
+/home/d669d153/work/beast.2.7.1/beast/bin/beast -threads 15 rep$SLURM_ARRAY_TASK_ID.xml
+
